e95df945fce40654998205550f0dcaa57430ccd6
issbah <53100816+issbah@users.noreply.github.com>
2019-07-19 15:55:58 -0700

    Add files via upload

--- DJ.js
+++ DJ.js
@@ -1,3 +1,4 @@
+
 function playlists() {
 	document.getElementById("demo").innerHTML = "This is my personal playlist and all these songs are pretty great so go ahead and click them and play em!";
 }
